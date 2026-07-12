b8cf185f38b0f6878a230cb765a9030f978331d5
Prateek Trivedi <prateektrivedi300@gmail.com>
2026-07-12 17:57:14 +0530

    Implement right rotation of array by one position

---
+++ Arrays/RightRotateArray.java
@@ -0,0 +1,19 @@
+public class RightRotateArray {
+    public static void main (String [] args){
+         int arr [] = {1, 2, 3, 4, 5};
+         int temp = arr[arr.length-1];
+
+         for(int i= arr.length-1 ; i > 0 ; i--){
+            arr[i] = arr[i-1];
+
+
+         }
+         arr[0]= temp;
+
+         System.out.println("Printing The Array by shifting one element to the right ");
+         for(int i = 0 ;  i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+         }
+    }
+    
+}
